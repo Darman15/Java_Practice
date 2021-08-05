11875aa0517406e14384f8a4a36ed2c29482a789
Benjamin Parks <Benparks101@gamil.comgit config --global user.email Benparks101@gamil.com>
2021-08-05 18:49:26 -0500

    very basic booleans practice

---
+++ Java/booleans.java
@@ -0,0 +1,23 @@
+public class booleans {
+
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("Test");
+        // Boolean values
+
+        boolean isJavaFun = true;
+        boolean isFishGood = false;
+
+        System.out.println(isJavaFun);
+        System.out.println(isFishGood);
+
+        // Boolean expressions
+
+        int x = 112;
+        int y = 155;
+
+        System.out.println(y < x);
+        System.out.println(x == 112);
+    }
+
+}
